fe55862656094602f65811c1a9d9c3530c507c0b
RaulVaor <raulvaor51@gmail.com>
2023-05-22 16:49:30 -0500

    Inicio del proyecto

---
+++ src/test/java/com/cibertec/DAWI_CL2_VargasRaul/DawiCl2VargasRaulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAWI_CL2_VargasRaul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl2VargasRaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
